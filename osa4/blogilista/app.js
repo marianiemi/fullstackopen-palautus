6eac7970cdb3cc6bb0a3033a73dfb90312d925c8
marianiemi <maria1niemi@gmail.com>
2026-01-05 22:13:10 +0200

    osa 4 tehtävä 4.2 utils/middleware added

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,11 +1,16 @@
+// Imports
 const express = require("express");
 const mongoose = require("mongoose");
+
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
 
+// App initialization
 const app = express();
 
+// Database connection
 logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
@@ -14,10 +19,19 @@ mongoose
     logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error("error connection to MongoDB:", error.message);
+    logger.error("error connecting to MongoDB:", error.message);
   });
 
+// Middleware
 app.use(express.json());
+app.use(middleware.requestLogger);
+
+// Routes
 app.use("/api/blogs", blogsRouter);
 
+// Error handling
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+// Export app
 module.exports = app;
