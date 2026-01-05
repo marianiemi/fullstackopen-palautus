37a9f788653179ae113711231a858994a5b663eb
marianiemi <maria1niemi@gmail.com>
2026-01-05 22:00:29 +0200

    osa4 tehtävä 4.2 utils/config added

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,8 +1,7 @@
-require("dotenv").config();
 const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-const PORT = process.env.PORT || 3003;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
