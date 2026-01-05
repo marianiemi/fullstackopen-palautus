0121a75f5efdac8cb5d341705a751c98927fa545
marianiemi <maria1niemi@gmail.com>
2026-01-05 21:49:33 +0200

    osa4 tehtävä 4.2 code split:app, model and controller

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,25 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blogs");
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI, { family: 4 });
 
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
